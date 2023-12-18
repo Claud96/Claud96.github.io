729c3d8dedf60afc14ba70d117e946286d1a9886
Claud96 <calize22@gmail.com>
2023-12-18 16:16:23 -0500

    updated demothree.js
    fixing JS hopefully.

--- DemoThree.js
+++ DemoThree.js
@@ -1,7 +1,7 @@
 let board = [
-	["O", "", "X"],
-	["O", "X", ""],
-	["X", "O", ""],
+    ["O", "", "X"],
+    ["O", "X", ""],
+    ["X", "O", ""],
 ];
 
 console.log(board[2][2]);
@@ -10,41 +10,41 @@ const allButtons = [...document.getElementsByTagName("button")];
 console.log(allButtons);
 
 for (const button of allButtons) {
-	// console.log(button);
-	button.onclick = () => {
-		if (button.innerHTML !== "&nbsp;") {
-			alert("CHEATER!");
-			return;
-		}
-		button.innerHTML = "X";
-		const emptyButtons = allButtons.filter(
-			(button) => button.innerHTML === "&nbsp;"
-		);
-		console.log(emptyButtons);
-		let randomButton =
-			emptyButtons[Math.floor(Math.random() * emptyButtons.length)];
-		if (!randomButton) {
-			alert("Board is filled");
-		} else {
-			document.getElementById("status").innerHTML =
-				"Computer is thinking";
-			setTimeout(() => {
-				randomButton.innerHTML = "O";
-				document.getElementById("status").innerHTML = "Your turn";
-			}, 500);
-		}
+    // console.log(button);
+    button.onclick = () => {
+        if (button.innerHTML !== "&nbsp;") {
+            alert("CHEATER!");
+            return;
+        }
+        button.innerHTML = "X";
+        const emptyButtons = allButtons.filter(
+            (button) => button.innerHTML === "&nbsp;"
+        );
+        console.log(emptyButtons);
+        let randomButton =
+            emptyButtons[Math.floor(Math.random() * emptyButtons.length)];
+        if (!randomButton) {
+            alert("Board is filled");
+        } else {
+            document.getElementById("status").innerHTML =
+                "Computer is thinking";
+            setTimeout(() => {
+                randomButton.innerHTML = "O";
+                document.getElementById("status").innerHTML = "Your turn";
+            }, 500);
+        }
 
-		//pick a random button and check to see if it's empty, if not loop 1000x
-		// for (let buttonChecker = 0; buttonChecker < 1000; buttonChecker++) {
-		// 	let randomButton =
-		// 		allButtons[Math.floor(Math.random() * allButtons.length)];
+        //pick a random button and check to see if it's empty, if not loop 1000x
+        // for (let buttonChecker = 0; buttonChecker < 1000; buttonChecker++) {
+        // 	let randomButton =
+        // 		allButtons[Math.floor(Math.random() * allButtons.length)];
 
-		// 	if (randomButton.innerHTML === "&nbsp;") {
-		// 		randomButton.innerHTML = "O";
-		// 		break;
-		// 	}
-		// 	console.log(buttonChecker);
-		// 	// console.log(randomButton);
-		// }
-	};
+        // 	if (randomButton.innerHTML === "&nbsp;") {
+        // 		randomButton.innerHTML = "O";
+        // 		break;
+        // 	}
+        // 	console.log(buttonChecker);
+        // 	// console.log(randomButton);
+        // }
+    };
 }
